d59fe1077422553fdbc39527e4251065301ecb2c
Anthony Nichols <hi@anthonynichols.me>
2021-03-11 14:36:18 -0600

    add easy folder access exports to zsh

--- zsh/local-exports.zsh
+++ zsh/local-exports.zsh
@@ -12,3 +12,10 @@ export DEFAULT_HOSTNAME="zenodotus.local"
 
 # Setup for nvm because nvm and node are being installed
 export NVM_DIR="$HOME/.nvm"
+
+# Easy folder access
+export MEDIADIR=/mnt/user/tank/media
+export MOVIEDIR=$MEDIADIR/movies
+export MUSICDIR=$MEDIADIR/music
+export TORRENTS=/mnt/user/tank/downloads/torrents
+export TVDIR=$MEDIADIR/tvshows
